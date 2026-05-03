12a949457e3f3d4b28c24a5f353d7e807de95be6
IT21101545 <it24101545@my.sliit.lk>
2026-05-03 15:37:38 +0530

    updated files

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { getCart, addToCart, updateCartItem, removeFromCart, clearCart } = require('../controllers/cartController');
+const { getCart, addToCart, updateCartItem, removeFromCart, clearCart } = require('../controllers/CartController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get   ('/',                    protect, getCart);
